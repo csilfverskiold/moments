afbc423c44f56b6747ad1e19dfafee59e622c0ea
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-15 13:46:34 +0000

    Add contexts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/Container';
-import {Route, Switch} from 'react-router-dom';
-import './api/axiosDefaults';
-import SignUpForm from './pages/auth/SignUpForm';
-import SignInForm from './pages/auth/SignInForm';
+import styles from "./App.module.css";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
+
 
 function App() {
+
+
   return (
-    /** The styles object here relates to the  name we gave our import at the top,  
+        <div className={styles.App}>
+          <NavBar />
+          <Container className={styles.Main}>
+            <Switch>
+              <Route exact path="/" render={() => <h1>Home page</h1>} />
+              <Route exact path="/signin" render={() => <SignInForm />} />
+              <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route render={() => <p>Page not found!</p>} />
+            </Switch>
+          </Container>
+        </div>
+  );
+}
+
+export default App;
+
+
+/** The styles object here relates to the  name we gave our import at the top,  
 and the .App here relates to the .App class we set  in our css file. */
-/** The Switch holds all our Routes,  and renders a given component when  
+    /** The Switch holds all our Routes,  and renders a given component when  
 a Route path matches the current URL. The render prop on our Route component  
 accepts a function that returns a component to  be rendered when the Route path is matched.
 The path prop is the browser url  the user will be at when they see  
 the component in our render prop. The “exact” prop tells the route to  
 only render its component when the  url entered is exactly the same. */
-    <div className={styles.App}>
-      <NavBar />
-      <Container className={styles.Main}>
-        <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>}/>
-          <Route exact path="/signin" render={() => <SignInForm />}/>
-          <Route exact path="/signup" render={() => <SignUpForm />}/>
-          <Route render={() => <p>Page not found!</p>}/>
-        </Switch>
-      </Container>
-    </div>
-  );
-}
-
-export default App;
