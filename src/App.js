fcf414bc07b2570ad0c830a80bb38272e9e22993
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-15 08:32:13 +0000

    Add custom styles to App and NavBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import styles from './App.module.css';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    /** The styles object here relates to the  name we gave our import at the top,  
+and the .App here relates to the .App class we set  in our css file. */
+    <div className={styles.App}>
       <NavBar />
     </div>
   );
